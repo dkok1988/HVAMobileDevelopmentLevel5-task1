a04a776cb8d0d05f7f254af9fec6a83e7f3def8c
Dave Kok <dave.kok@nn.nl>
2020-11-29 15:01:42 +0100

    added final changes

---
+++ app/src/main/java/com/example/madlevel5task1/AddNoteFragment.kt
@@ -0,0 +1,59 @@
+package com.example.madlevel5task1
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_add_note.*
+
+
+class AddNoteFragment : Fragment() {
+
+    private val viewModel: NoteViewModel by viewModels()
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_add_note, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btnSave.setOnClickListener {
+            saveNote()
+        }
+
+        observeNote()
+    }
+
+    private fun observeNote() {
+        viewModel.note.observe(viewLifecycleOwner, Observer {
+                note  ->
+            note?.let {
+                tilNoteTitle.editText?.setText(it.title)
+                tilNoteText.editText?.setText(it.text)
+            }
+
+        })
+
+        viewModel.error.observe(viewLifecycleOwner, Observer { message ->
+            Toast.makeText(activity, message, Toast.LENGTH_SHORT).show()
+        })
+
+        viewModel.success.observe(viewLifecycleOwner, Observer {     success ->
+            findNavController().popBackStack()
+        })
+    }
+
+    private fun saveNote() {
+        viewModel.updateNote(tilNoteTitle.editText?.text.toString(), tilNoteText.editText?.text.toString())
+    }
+
+}
